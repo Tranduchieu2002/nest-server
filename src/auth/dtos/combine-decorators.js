041ccc50ac26f74c2c26508edd209cdb2ce682fb
Trần Hiếu <cuhieu04112002@gmail.com>
2022-11-01 20:55:58 +0700

    remove js files

--- src/auth/dtos/combine-decorators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.AuthDecorators = void 0;
-var common_1 = require("@nestjs/common");
-var AuthDecorators = function (roles) {
-    return (0, common_1.applyDecorators)();
-};
-exports.AuthDecorators = AuthDecorators;
--- src/constants/roles.js
+++
@@ -1,6 +0,0 @@
-var ROLES;
-(function (ROLES) {
-    ROLES["superAdmin"] = "SUPER-ADMIN";
-    ROLES["admin"] = "ADMIN";
-    ROLES["guest"] = "GUEST";
-})(ROLES || (ROLES = {}));
