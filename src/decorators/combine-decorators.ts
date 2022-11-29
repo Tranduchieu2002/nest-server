d285eaf7c3225a2d50f95972219e60c0d2fe8bb7
Trần Hiếu <cuhieu04112002@gmail.com>
2022-11-30 01:51:17 +0700

    config option public route

--- src/decorators/combine-decorators.ts
+++ src/decorators/combine-decorators.ts
@@ -1,11 +1,14 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
-import { ROLES } from '../constants/roles';
+import { RoleEnum } from '../constants/roles';
 import { JwtGuard } from '../modules/auth/jwt.guard';
 import { LocalAuthGuard } from '../modules/auth/local-auth.guard';
 
-export const AuthDecorators = (roles: ROLES[]) => {
+export const AuthDecorators = (
+  roles: RoleEnum[] = [],
+  options?: Partial<{ isPublic: boolean }>,
+) => {
   return applyDecorators(
     SetMetadata('roles', roles),
-    UseGuards(JwtGuard, LocalAuthGuard /* RolesGuard */),
+    UseGuards(JwtGuard(options), LocalAuthGuard /* RolesGuard */),
   );
 };
