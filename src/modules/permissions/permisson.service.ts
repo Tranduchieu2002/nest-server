81701d8b5e2673d15b246e8de7ac37cee24351e9
Trần Hiếu <cuhieu04112002@gmail.com>
2022-12-06 01:01:53 +0700

    feat:  get all  roles user

--- src/modules/permissions/permisson.service.ts
+++ src/modules/permissions/permisson.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RoleEntity } from '../../modules/role/role.entity';
+import { UserDto } from '../../modules/user/dtos/user.dto';
+import { UserEntity } from '../../modules/user/user.entity';
 import { PermissionsEntity } from './permission.entity';
 
 @Injectable()
 export class PermissionsSevice {
   constructor(
     @InjectRepository(PermissionsEntity)
-    private permissionsEntity: PermissionsEntity,
+    private permissionsRepository: Repository<PermissionsEntity>,
+    @InjectRepository(RoleEntity)
+    private rolesRepository: Repository<RoleEntity>,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
   ) {}
-  generatePermissions() {}
+  async generatePermissions(user: UserDto) {
+    const userByRoles = await this.userRepository
+      .createQueryBuilder('user')
+      .where({ id: user.id })
+      .select([
+        'user.id',
+        'roles.id',
+        'roles.name',
+        'permissions.name',
+        'permissions.id',
+        'permissions.model',
+      ])
+      .leftJoin('user.roles', 'roles') // bar is the joined table
+      .leftJoin('roles.permissions', 'permissions')
+      .getOne();
+    return userByRoles?.roles;
+  }
 }
