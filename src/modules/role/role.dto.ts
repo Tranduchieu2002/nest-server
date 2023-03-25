05032f0d3343ea989b24e65681ed0d14e89dace7
Tran Hieu <hieutd@melisoft.vn>
2023-03-25 20:32:01 +0700

    add permission dto in roledto

--- src/modules/role/role.dto.ts
+++ src/modules/role/role.dto.ts
@@ -1,17 +1,21 @@
 import { IsString } from "class-validator";
 import { AbstractTranslationDto } from "../base/base.dto";
 import { PermissionsDto } from "../permissions/permission.dto";
-import { PermissionsEntity } from "../permissions/permission.entity";
 import { RoleEntity } from "./role.entity";
 
 export type RoleDtoOptions = Partial<{ isActive: boolean , isHasPermissions: boolean}>;
 
 export class RoleDto extends AbstractTranslationDto {
   @IsString()
-  name: string;  
+  name: string;
+
+  permissions: PermissionsDto[]
   constructor(roleE: RoleEntity, options?: RoleDtoOptions) {
     super(roleE);
     this.name = roleE.name;
+    if(options?.isHasPermissions) {
+      this.permissions = roleE.permissions.map(p => p.toDto())
+    }
   }
   
 }
